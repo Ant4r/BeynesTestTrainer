2ac7f458c0185f0ffb50191f346881feb7541bd8
Ant4r <nide38@gmail.com>
2021-05-31 04:08:54 +0200

    project base

---
+++ src/beynesTest/Model/BeynesTestModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace BeynesTestTrainer.Model
+{
+    public class BeynesTestModel
+    {
+        public List<DefinedColor> ColorList { get; set; }
+        
+        public int ShowTime { get; set; }
+        public int OffTime { get; set; }
+
+        public BeynesTestModel()
+        {
+            ColorList = new();
+        }
+    }
+}
---
+++ src/beynesTest/Model/DefinedColor.cs
@@ -0,0 +1,22 @@
+﻿namespace BeynesTestTrainer.Model
+{
+    public class DefinedColor
+    {
+        public string Name { get; set; }
+        public byte R { get; set; }
+        
+        public byte G { get; set; }
+        
+        public byte B { get; set; }
+
+
+        public DefinedColor(string name, byte r, byte g, byte b)
+        {
+            Name = name;
+            R = r;
+            G = g;
+            B = b;
+        }
+
+    }
+}
